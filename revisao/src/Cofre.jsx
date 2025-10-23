9b804b9ba4759477b39b99bf3ada34d6d705b986
Lucas Zampronio <lucas.zampronio.silva@gmail.com>
2025-10-22 22:08:53 -0300

    t

--- revisao/src/Cofre.jsx
+++ revisao/src/Cofre.jsx
@@ -1,13 +1,35 @@
 import Tecla from "./Tecla";
+import { useState } from "react";
 
 export default function Cofre(props) {
+    const senhaatual = props.senha.split('').map((valor,indice)=>({id:indice+1,valorDigitado:0,valorCorreto:Number(valor)}));
+    console.log(senhaatual);
+    const [digitos,setDigitos] = useState(senhaatual);
+
+
+
+    function atualizaDigito(valor, id){
+
+        let novosDigitos;
+        novosDigitos = digitos.map(digito=>{
+            if(digito.id == id){
+                let novodigito = {...digito};
+                novodigito.valorDigitado = valor;
+                return novodigito;
+            }else{
+                return digito;
+            }
+        });
+
+        setDigitos(novosDigitos);
+        }
+
+        
 
     return<> 
     Senha informada : {props.senha} 
-    
-    <Tecla/>
-    <Tecla/>
-    <Tecla/>
-    <Tecla/>
+    <br /><br />
+    {digitos.map(digito=>
+    <Tecla onChange={(valor)=>atualizaDigito(valor,digito.id)}/>)}
     </>
-}
+} 
--- revisao/src/Tecla.jsx
+++ revisao/src/Tecla.jsx
@@ -1,5 +1,35 @@
+import { useState } from "react";
+import adicionar from './assets/adicionar.png';
+import remover from './assets/remover.png';
+import proibido from './assets/proibido.png';
+
+ 
 export default function Tecla(props) {
-    return <>
-    Tecla
-    </>
+
+    const[valor,setValor] = useState(0);
+    
+    function diminuir(){
+        let novoValor = valor 
+        novoValor--;
+        if(novoValor < 0){
+            novoValor = 0;
+        }
+        setValor(novoValor);
+        props.onChange(novoValor);
+    }
+
+    function aumentar(){
+        let novoValor = valor 
+        novoValor++;
+        setValor(novoValor);
+        props.onChange(novoValor);
+    }
+    return <spam style={{color:'red',cursor:'pointer',fontSize:'40px'}} >
+    {valor>0?<img onClick={diminuir} src={remover} />:<img src={proibido} />}
+
+
+    {valor}
+    
+    {valor < 9?<img onClick={aumentar} src={adicionar} />:<img src={proibido} />}
+    </spam>
 }
