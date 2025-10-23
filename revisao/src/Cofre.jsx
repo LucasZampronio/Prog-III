eca459a60692ab961b3d12924439e880909ca05d
Lucas Zampronio <lucas.zampronio.silva@gmail.com>
2025-10-22 22:25:07 -0300

    t

--- revisao/src/Cofre.jsx
+++ revisao/src/Cofre.jsx
@@ -3,9 +3,25 @@ import { useState } from "react";
 
 export default function Cofre(props) {
     const senhaatual = props.senha.split('').map((valor,indice)=>({id:indice+1,valorDigitado:0,valorCorreto:Number(valor)}));
+    const [tentativas,setTentativas] = useState(0);
+
+    
+
     console.log(senhaatual);
     const [digitos,setDigitos] = useState(senhaatual);
 
+    function abrir(){ 
+        let errado = digitos.find(digito=> digito.valorDigitado!== digito.valorCorreto);
+        if(errado){
+            alert('Senha incorreta!');
+            let novastentativas = tentativas;
+            novastentativas++;
+            setTentativas(novastentativas);
+        }else{
+            alert('Cofre aberto com sucesso!');
+        }
+
+     }
 
 
     function atualizaDigito(valor, id){
@@ -24,12 +40,14 @@ export default function Cofre(props) {
         setDigitos(novosDigitos);
         }
 
-        
+
 
     return<> 
     Senha informada : {props.senha} 
     <br /><br />
     {digitos.map(digito=>
     <Tecla onChange={(valor)=>atualizaDigito(valor,digito.id)}/>)}
+    <br />
+    <button onClick={abrir}>Abrir</button>
     </>
 } 
